bd3fcbe1da89491393fc89e684a92ed0e722702a
Abhishek Sawai <155893906+Ajsawai2@users.noreply.github.com>
2024-03-06 19:05:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,60 @@
-let navLinks = document.querySelectorAll('div header nav a');
+let navLinks = document.querySelectorAll("div header nav a");
 
-let but1=document.querySelector("#butA");
-let but2=document.querySelector("#butB");
-let but3=document.querySelector("#butC");
-let but4=document.querySelector("#butD");
-let his2=document.querySelector(".history");
-let user=document.querySelector(".user");
-user.addEventListener("click",()=>{
-  navLinks[1].style.color="#0ef";
-  navLinks[0].style.color="#fff";
-})
+let but1 = document.querySelector("#butA");
+let but2 = document.querySelector("#butB");
+let but3 = document.querySelector("#butC");
+let but4 = document.querySelector("#butD");
+let his2 = document.querySelector(".history");
+let user = document.querySelector(".user");
+user.addEventListener("click", () => {
+  navLinks[1].style.color = "#0ef";
+  navLinks[0].style.color = "#fff";
+});
 
-his2.addEventListener("click",()=>{
-  navLinks[2].style.color="#0ef";
-  navLinks[0].style.color="#fff";
-})
+his2.addEventListener("click", () => {
+  navLinks[2].style.color = "#0ef";
+  navLinks[0].style.color = "#fff";
+});
 
-but1.addEventListener("click",()=>{
-  navLinks[0].style.color="#0ef";
-  navLinks[1].style.color="#fff";
-  navLinks[2].style.color="#fff";
-  navLinks[3].style.color="#fff";
-})
+but1.addEventListener("click", () => {
+  navLinks[0].style.color = "#0ef";
+  navLinks[1].style.color = "#fff";
+  navLinks[2].style.color = "#fff";
+  navLinks[3].style.color = "#fff";
+});
 
-but2.addEventListener("click",()=>{
-  navLinks[0].style.color="#fff";
-  navLinks[1].style.color="#0ef";
-  navLinks[2].style.color="#fff";
-  navLinks[3].style.color="#fff";
-})
+but2.addEventListener("click", () => {
+  navLinks[0].style.color = "#fff";
+  navLinks[1].style.color = "#0ef";
+  navLinks[2].style.color = "#fff";
+  navLinks[3].style.color = "#fff";
+});
 
-but3.addEventListener("click",()=>{
-  navLinks[0].style.color="#fff";
-  navLinks[1].style.color="#fff";
-  navLinks[2].style.color="#0ef";
-  navLinks[3].style.color="#fff";
-})
-but4.addEventListener("click",()=>{
-    navLinks[0].style.color="#fff";
-    navLinks[1].style.color="#fff";
-    navLinks[2].style.color="#fff";
-    navLinks[3].style.color="#0ef";
-})
+but4.addEventListener("click", () => {
+  navLinks[0].style.color = "#fff";
+  navLinks[1].style.color = "#fff";
+  navLinks[2].style.color = "#0ef";
+  //  navLinks[3].style.color="#0ef";
+});
+
+let bs = () => {
+  setInterval(() => {
+    document.querySelector("body").style.backgroundImage = "";
+
+    let bx = Math.floor(Math.random() * 100000000);
+    return bx;
+  }, 0.1);
+};
+
+let signup = document.querySelector(".fod");
+
+setTimeout(() => {
+  signup.style.display = "flex";
+}, 3000);
+
+let close = document.querySelector("#bud");
+close.addEventListener("click", () => {
+  setTimeout(() => {
+    signup.style.display = "none";
+  }, 0.8);
+});
